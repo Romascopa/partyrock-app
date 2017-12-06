21876b44505eed51cc836c65fe6e9e9904135b2c
Lance Robbins <riotchild25@gmail.com>
2017-12-06 11:06:16 -0500

    partyrock done

---
+++ PartyRockApp/Controller/MainVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Lance Robbins on 11/28/17.
+//  Copyright © 2017 Appcation. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+  
+
+
+}
+
---
+++ PartyRockApp/Model/PartyRock.swift
@@ -0,0 +1,9 @@
+//
+//  PartyRock.swift
+//  PartyRockApp
+//
+//  Created by Lance Robbins on 11/29/17.
+//  Copyright © 2017 Appcation. All rights reserved.
+//
+
+import Foundation
